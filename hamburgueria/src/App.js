2b8553d54c8b949ccb22d47e4ffc56a2e11a0d19
jpedrofp <ferreirapillon.joaop@gmail.com>
2021-11-08 14:55:50 -0300

    introduced verification

--- hamburgueria/src/App.js
+++ hamburgueria/src/App.js
@@ -1,27 +1,24 @@
-
 import MenuContainer from "./components/MenuContainer";
 import { useState } from "react";
 
 function App() {
   const [products, setProducts] = useState([
-    { id: 1, name: 'Hamburguer', category: 'Sanduíches', price: 14.00},
-    { id: 2, name: 'X-Burguer', category: 'Sanduíches', price: 16.00 },
-    { id: 3, name: 'Big Kenzie', category: 'Sanduíches', price: 18.00 },
-    { id: 4, name: 'Fanta Guaraná', category: 'Bebidas', price: 5.00},
-    { id: 5, name: 'Coca', category: 'Bebidas', price: 4.99},
-    { id: 6, name: 'Milkshake', category: 'Bebidas', price: 4.99},
+    { id: 1, name: "Hamburguer", category: "Sanduíches", price: 14.0 },
+    { id: 2, name: "X-Burguer", category: "Sanduíches", price: 16.0 },
+    { id: 3, name: "Big Kenzie", category: "Sanduíches", price: 18.0 },
+    { id: 4, name: "Fanta Guaraná", category: "Bebidas", price: 5.0 },
+    { id: 5, name: "Coca", category: "Bebidas", price: 4.99 },
+    { id: 6, name: "Milkshake", category: "Bebidas", price: 4.99 },
   ]);
-
 
   const [filteredProducts, setFilteredProducts] = useState([]);
 
   function showProducts(input) {
     const filtrados = products.filter((product) => {
-
-      for (let i=0;i<=product.name.length;i++){
-      if (product.name === input) return true
+      for (let i = 0; i <= product.name.length; i++) {
+        if (product.name === input) return true;
       }
-      return false
+      return false;
     });
     setFilteredProducts([...filtrados]);
   }
@@ -33,23 +30,23 @@ function App() {
       return selected.id === prod;
     });
 
+    if (currentSale.includes(sale)) {
+      return;
+    }
+
     setCurrentSale([...currentSale, sale]);
   }
 
-
-
   return (
     <div className="App">
-    
-        <MenuContainer
-          showProducts={showProducts}
-          filteredProducts={filteredProducts}
-          products={products}
-          handleClick={handleClick}
-          currentSale={currentSale}
-          setCurrentSale={setCurrentSale}
-        ></MenuContainer>
-
+      <MenuContainer
+        showProducts={showProducts}
+        filteredProducts={filteredProducts}
+        products={products}
+        handleClick={handleClick}
+        currentSale={currentSale}
+        setCurrentSale={setCurrentSale}
+      ></MenuContainer>
     </div>
   );
 }
--- hamburgueria/src/components/TotalSale.jsx
+++ hamburgueria/src/components/TotalSale.jsx
@@ -1,7 +1,7 @@
 import "./styles.css";
 import Cart from "./Cart";
 
-const TotalSale = ({ currentSale, setCurrentSale}) => {
+const TotalSale = ({ currentSale, setCurrentSale }) => {
   const accumulator = [];
   const cartSum = currentSale?.reduce((acc, currentValue) => {
     if (accumulator.includes(currentValue.id)) {
@@ -11,55 +11,36 @@ const TotalSale = ({ currentSale, setCurrentSale}) => {
     return acc + currentValue.price;
   }, 0);
 
-  function verify(sale) {
-    const accumulator = [];
-    if (accumulator.length === 0) {
-      accumulator.push(sale.id);
-      return false;
-    }
-    if (accumulator.includes(sale.id)) {
-      accumulator.push(sale.id);
-      return true;
-    }
-    accumulator.push(sale.id);
-    return false;
-  }
-
   const cartSumDec = (Math.round(cartSum * 100) / 100).toFixed(2);
 
   function removeAll() {
-    setCurrentSale([])
+    setCurrentSale([]);
   }
-
+  console.log(currentSale);
   return (
     <div className="total">
-        <div className="cartContainer">
+      <div className="cartContainer">
         <h4>Carrinho de compras</h4>
-        </div>
+      </div>
       <div className="cartCard">
-      
-
-        {currentSale.map((sale) =>
-          verify(sale) ? null : (
-            <li key={sale.id}>
-              <Cart sale={sale} />
-            </li>
-          )
-        )}
+        {currentSale.map((sale) => (
+          <li key={sale.id}>
+            <Cart sale={sale} />
+          </li>
+        ))}
       </div>
       <hr></hr>
       <div className="finish">
         <span> Total </span>
         <h6> R$ {cartSumDec}</h6>
-        
       </div>
       <div className="rem">
-      <button className="remove" onClick={removeAll}>Remover todos</button>
+        <button className="remove" onClick={removeAll}>
+          Remover todos
+        </button>
       </div>
-     
     </div>
   );
 };
-
 
 export default TotalSale;
